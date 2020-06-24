aa0121dfbac51a9fcee4cb3fd473259c4bdb0f4c
William's MBP <modeste.william.s@gmail.com>
2020-06-24 15:31:10 +0200

    ajout du jwt

--- src/main/java/com/mowil/ats/configuration/SecurityConfigurer.java
+++ src/main/java/com/mowil/ats/configuration/SecurityConfigurer.java
@@ -2,11 +2,15 @@ package com.mowil.ats.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.mowil.ats.services.UtilisateurDetailsService;
 
@@ -15,14 +19,41 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UtilisateurDetailsService utilisateurDetailService;
+
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(utilisateurDetailService);
+		auth.userDetailsService(utilisateurDetailService).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder(){
-		PasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder;
+	public PasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder();
+	}
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+	return super.authenticationManagerBean();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+	// We don't need CSRF for this example
+	httpSecurity.csrf().disable()
+	// dont authenticate this particular request
+	.authorizeRequests().antMatchers("/authenticate").permitAll().
+	// all other requests need to be authenticated
+	anyRequest().authenticated().and().
+	// make sure we use stateless session; session won't be used to
+	// store user's state.
+	exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+	.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	// Add a filter to validate the tokens with every request
+	httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
