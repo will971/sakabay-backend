9306da8a857d9b6d330f5e9f3f660ec3a0a2ccb8
William's MBP <modeste.william.s@gmail.com>
2020-07-01 03:32:35 +0200

    suppression d'une requête inutilisé

--- src/main/java/com/mowil/ats/dao/repositories/ProfessionnelRepository.java
+++ src/main/java/com/mowil/ats/dao/repositories/ProfessionnelRepository.java
@@ -1,17 +1,12 @@
-package com.mowil.ats.dao;
-
-import com.mowil.ats.entities.Professionnel;
-
-import java.util.Optional;
+package com.mowil.ats.dao.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.mowil.ats.dao.entities.Professionnel;
 
 @RepositoryRestResource
 public interface ProfessionnelRepository extends JpaRepository<Professionnel,Long> {
 
-	@Query(" select p from Professionnel p " +
-            " where p.mail=?1")
-	public Optional<Professionnel> findProfessionnelByUsername(String username);
+	
 }
